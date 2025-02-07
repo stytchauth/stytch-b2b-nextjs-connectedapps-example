aa03fbe3e5b2196cf3f8a4c9d9411b174ebb583d
Matthew Bramlage <mattbramlage@stytch.com>
2025-02-07 15:26:41 -0800

    Log in if not already logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+'use client'
 import { useStytchMemberSession } from '@stytch/nextjs/b2b';
+import Login from '@/components/Login';
 
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Index() {
+
+  const { session, isInitialized } = useStytchMemberSession();
+
+  var loginStatus;
+  if (isInitialized && session) {
+    loginStatus = <div>Logged in!</div>
+  } else {
+    loginStatus = <div>
+      Not logged in. Please log in first.
+      <Login />
+    </div>
+  }
+
   return (
     <div>
       <h2 className="text-center">Welcome to the Stytch Connected Apps demo!</h2>
@@ -14,6 +29,7 @@ export default function Index() {
           <li>To act as the app supplying the user identity information, the Identity Provider.</li>
         </ol>
         <br />
+        {loginStatus}
        </div>
       <br />
       <div className="columns-2">
---
+++ src/components/Login.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import React from 'react';
+import { StytchB2B } from '@stytch/nextjs/b2b';
+import { discoveryConfig, discoveryStyles, organizationConfig } from '@/lib/stytchConfig';
+import './Login.css';
+
+/*
+ * Login configures and renders the StytchLogin component which is a prebuilt UI component for auth powered by Stytch.
+ *
+ * This component accepts style, config, and callbacks props. To learn more about possible options review the documentation at
+ * https://stytch.com/docs/b2b/sdks/ui-config
+ */
+
+const Login = () => {
+
+  return (
+    <div className="centered-login">
+      <StytchB2B config={discoveryConfig} styles={discoveryStyles} />
+    </div>
+  );
+};
+
+export default Login;
